2a316fe499ae86579008a1e9afa3b6ca8e90fd09
laefy13 <karloosss13@gmail.com>
2023-11-28 05:41:37 +0000

    empty cache

--- train.py
+++ train.py
@@ -390,6 +390,7 @@ def _parse_args():
 
 
 def main():
+    torch.cuda.empty_cache()
     utils.setup_default_logging()
     args, args_text = _parse_args()
 
@@ -854,6 +855,8 @@ def main():
 
     if best_metric is not None:
         _logger.info('*** Best metric: {0} (epoch {1})'.format(best_metric, best_epoch))
+        
+    torch.cuda.empty_cache()
 
 
 def train_one_epoch(
